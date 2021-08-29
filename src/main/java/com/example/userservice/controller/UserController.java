9d98d97e756be5806214238d5258e356f5ae2377
yoonmin-kim <rladbsals23@gmail.com>
2021-08-29 21:35:23 +0900

    feat : Users Microservice - H2 데이터베이스 연동
    1.4.xxx 버전에서는 최초 기동시 자동으로 db생성을 해주지 않기 때문에(보안문제로 인해)
    1.3.xxx 버전으로 dependency 해주었다.
    
    h2데이터베이스 전용 웹콘솔을 접근허용 및 url설정을 해준다.
    spring.h2.console.enabled=true
    spring.h2.console.settings.web-allow-others=true
    spring.h2.console.path=/h2-console

---
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class UserController {
+
+    private final Environment env;
+    private final Greeting greeting;
+
+    @GetMapping("/health-check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+}
